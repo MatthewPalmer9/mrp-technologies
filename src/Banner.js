b433daaea8ca86cf0d0c99c3b2d19cfff81cefe6
MacBookPro <macbookpro@MacBookPros-MBP.attlocal.net>
2020-06-11 18:53:20 -0500

    Updating details for header.

--- src/Banner.js
+++ src/Banner.js
@@ -5,7 +5,7 @@ export default class Banner extends Component {
     render () {
         return(
             <div className="banner-container">
-                <div className="brand"><span className="head">Matthew Palmer</span>  <span className="subhead"><em>A passionate UX/UI Developer who specializes in JavaScript / React.js </em></span></div>
+                <div className="brand"><span className="head">Matthew Palmer</span>  <span className="subhead"><em>A passionate Full Stack Developer who specializes in the Front End with JavaScript / React.js </em></span></div>
                 <div className="download">
                     <div className="dl-btn">
                         <a href={resume} download>Resume <i className="fas fa-download"></i></a>
