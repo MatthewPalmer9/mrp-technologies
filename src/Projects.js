730feaa59dcd882c45908795ae4de6a625515058
MacBookPro <macbookpro@MacBookPros-MBP.attlocal.net>
2020-10-09 21:57:59 -0500

    Updated downloadable resume and added latest project, JS Slider

--- src/Projects.js
+++ src/Projects.js
@@ -3,6 +3,7 @@ import ComingSoon from './ComingSoon.jpg';
 import TheGentleman from './thegentleman.jpg';
 import Bowlicious from './bowlicious.png';
 import RgbGuess from './rgbguess.png';
+import JSSlider from './js-slider.png';
 
 export default class Projects extends Component {
     render() {
@@ -15,7 +16,7 @@ export default class Projects extends Component {
                             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Projects</h1>
                             <div className="h-1 w-20 bg-indigo-500 rounded"></div>
                         </div>
-                        <p className="lg:w-1/2 w-full leading-relaxed text-base">Below, you'll find a showcase for some of my most proudly developed projects. A cocktail of static UI landing pages and deeper projects utilizing technologies focused on swift development, responsive design and use of back end APIs for information storage. <strong>These projects are currently being rolled out. This section is under construction.</strong></p>
+                        <p className="lg:w-1/2 w-full leading-relaxed text-base">Below, you'll find a showcase for some of my most proudly developed projects. A cocktail of UI/UX, landing pages and deeper projects utilizing technologies focused on swift development, responsive design and use of back end APIs for information storage.</p>
                         </div>
                         <div className="flex flex-wrap -m-4">
                         <div className="xl:w-1/4 md:w-1/2 p-4">
@@ -44,10 +45,10 @@ export default class Projects extends Component {
                         </div>
                             <div className="xl:w-1/4 md:w-1/2 p-4">
                                 <div className="projectBox bg-gray-100 p-6 rounded-lg">
-                                    <img className="h-40 rounded w-full object-cover object-center mb-6" src={ComingSoon} alt="content"></img>
-                                    <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">TYPE</h3>
-                                    <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Coming Soon</h2>
-                                    <p className="leading-relaxed text-base">lorem ipsum</p>
+                                    <a target="_blank" rel="noopener noreferrer" href="https://mrp-carousel.vercel.app/"><img className="h-40 rounded w-full object-cover object-center mb-6" src={JSSlider} alt="content"></img></a>
+                                    <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">UI/UX</h3>
+                                    <h2 className="text-lg text-gray-900 font-medium title-font mb-4"><a target="_blank" rel="noopener noreferrer" href="https://mrp-carousel.vercel.app/">Image Slider / Carousel</a></h2>
+                                    <p className="leading-relaxed text-base">An image slider made in 100%, good 'ol HTML, CSS, and JavaScript</p>
                                 </div>
                             </div>
                         </div>
